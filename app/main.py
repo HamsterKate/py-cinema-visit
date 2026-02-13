61b71ef100b7a57a30721a19ead946d629fc1c4d
Kate <hamster.k.po@gmail.com>
2026-02-13 21:46:23 +0200

    movie in cinema_visit back to position #4

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
-        movie: str,
         customers: list[dict[str, str]],
         hall_number: int,
         cleaner: str,
+        movie: str,
 ) -> None:
     # Create Customer instances
     customer_instances = [
