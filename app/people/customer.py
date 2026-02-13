d61d86fd2ff26f9fa400220e119f6bd6e2b4380e
Kate <hamster.k.po@gmail.com>
2026-02-13 21:41:11 +0200

    corrections according to AI-mentor

--- app/people/customer.py
+++ app/people/customer.py
@@ -1,9 +1,7 @@
-
-
 class Customer:
     def __init__(self, name: str, food: str) -> None:
         self.name = name
         self.food = food
 
-    def watch_movie(self, movei: str) -> None:
-        print(f'{self.name} is watching "{movei}".')
+    def watch_movie(self, movie: str) -> None:
+        print(f'{self.name} is watching "{movie}".')
